c796defef82a0ce8a895541a9e0e3169b32f1641
PongpunSaengphon <pp_saeng@hotmail.com>
2024-11-10 22:50:55 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
-const allowedOrigins = ["http://localhost:8080", "http://172.20.10.4:8080", "http://10.203.70.225:8080"];
+const allowedOrigins = ["http://localhost:8080", "http://10.203.70.62:8080"];
 app.use(
   cors({
     origin: function (origin, callback) {
