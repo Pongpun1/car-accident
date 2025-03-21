2b9e9e80bd6e32efff717a8a2b628afe7cbbf3f9
ZofearZ <88421180+ZofearZ@users.noreply.github.com>
2025-03-21 22:02:33 +0700

    Update LoginUser.vue again

--- src/views/LoginUser.vue
+++ src/views/LoginUser.vue
@@ -74,7 +74,7 @@ export default {
       }
 
       try {
-        const response = await axios.post("http://34.126.127.208/:3000/users/login", {
+        const response = await axios.post("http://34.126.127.208:3000/users/login", {
           username: this.username,
           password: this.password,
         });
